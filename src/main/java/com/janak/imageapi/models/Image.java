a68baf7fc971444f59d8d17d5f718179b83b784b
janak <janakchy01@gmail.com>
2025-05-28 18:22:20 +0545

    feature:model:add Image model

---
+++ src/main/java/com/janak/imageapi/models/Image.java
@@ -0,0 +1,35 @@
+package com.janak.imageapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String imageName;
+
+    private String imageType;
+
+    private String imageSize;
+    @CreationTimestamp
+    private Date uploadedAt;
+
+
+}
