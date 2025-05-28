b4a98f889b5e057a753b47217f7b03febb1d2848
janak <janakchy01@gmail.com>
2025-05-28 18:23:39 +0545

    feature:repo:add Image Repository

---
+++ src/main/java/com/janak/imageapi/Repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.janak.imageapi.Repository;
+
+import com.janak.imageapi.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
